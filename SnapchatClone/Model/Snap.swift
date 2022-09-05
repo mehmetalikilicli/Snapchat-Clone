feeed5a56940abd7e77c3c0af0908edce76ba336
Mehmet Ali <58392243+mehmetalikilicli@users.noreply.github.com>
2022-09-05 18:34:55 +0300

    Finish Snapchat Clone

---
+++ SnapchatClone/Model/Snap.swift
@@ -0,0 +1,17 @@
+//
+//  Snap.swift
+//  SnapchatClone
+//
+//  Created by Mehmet Ali Kılıçlı on 31.08.2022.
+//
+
+import Foundation
+
+struct Snap {
+    
+    var username: String
+    var imageUrlArray: [String]
+    var date : Date
+    var timeDifference : Int
+    
+}
